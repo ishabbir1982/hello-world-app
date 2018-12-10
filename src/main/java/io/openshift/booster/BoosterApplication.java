b30943634659cfd3ae748b741eeea1bf338c9ebc
Imran Shabbir <ishabbir1982@gmail.com>
2018-12-10 15:06:35 +0100

    Update BoosterApplication.java

--- src/main/java/io/openshift/booster/BoosterApplication.java
+++ src/main/java/io/openshift/booster/BoosterApplication.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2016-2017 Red Hat, Inc, and individual contributors.
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (The "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
